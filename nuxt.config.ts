5cd5e289cb9502fbc69d4e44482cd5272940e820
fadlan18 <fadlan.id@email.com>
2025-07-26 07:41:30 +0800

    lengkap sudah aman tailwind 001

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+
+  modules: ['@nuxtjs/tailwindcss'],
+  css: ['@/assets/css/tailwind.css'],
+
   devtools: { enabled: true },
   routeRules: {
     // prerender index route by default
