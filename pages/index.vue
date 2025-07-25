29320f60bd5d650731f87cd2b5ed55dcfa6e05dc
fadlan18 <fadlan.id@email.com>
2025-07-25 19:35:17 +0800

    tambah index.vue halaman utama

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,6 @@
-
+<template>
+  <div>
+    <div class="bg-red-500 text-white p-4">Tes Tailwind</div>
+    <div style="background-color: #047857; color: white; padding: 1rem;">Tes Tanpa Tailwind</div>
+  </div>
+</template>
