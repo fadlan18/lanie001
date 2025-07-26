b20ad45379ba1f0777363947965d765880ee7f5f
fadlan18 <fadlan.id@email.com>
2025-07-26 08:34:37 +0800

    fix: ensure vercel deployment works

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 
+  nitro: {
+    preset: 'vercel'
+  },
+
   modules: ['@nuxtjs/tailwindcss'],
   css: ['@/assets/css/tailwind.css'],
 
